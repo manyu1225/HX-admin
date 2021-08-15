fb668f81fe5e96fe51ae39800ca25783206fbbf7
manyu1225 <manyu1225@gmail.com>
2021-08-15 23:13:10 +0800

    admin

--- app/assets/script/all.js
+++ app/assets/script/all.js
@@ -1,13 +1,2 @@
 
-$(document).ready(function (event) {
-    var $li = $('.product__tab__list li')
-    $($li.eq(0).addClass('active').find('a').attr('href')).siblings('.product__tab__item').hide()
-    $li.click(function (event) {
-        event.preventDefault();
-        $($(this).find('a').attr('href')).show().siblings('.product__tab__item').hide()
-        $(this).addClass('active').siblings('.active').removeClass('active')
-        $('html,body').animate({
-            scrollTop: 0
-        }, 500)
-    })
-})
+console.log('Hello!');
